78f826c6c1db6ba69105677d7c03e2050f737d01
aadil42 <adilkelawala@gmail.com>
2024-01-06 15:28:59 +0530

    remove skills

--- app/src/constant.js
+++ app/src/constant.js
@@ -11,42 +11,42 @@ const SKILLS = {
     '../../public/Images/Skills/Client/html.png',
     '../../public/Images/Skills/Client/css.png',
     '../../public/Images/Skills/Client/react.png',
-    '../../public/Images/Skills/Client/angular.png',
-    '../../public/Images/Skills/Client/vue.png',
-    '../../public/Images/Skills/Client/blaze.png'
+    // '../../public/Images/Skills/Client/angular.png',
+    // '../../public/Images/Skills/Client/vue.png',
+    // '../../public/Images/Skills/Client/blaze.png'
   ],
-  server: [
-    '../../public/Images/Skills/Server/java.png',
-    '../../public/Images/Skills/Server/python.png',
-    '../../public/Images/Skills/Server/node.png',
-    '../../public/Images/Skills/Server/meteor.png',
-    '../../public/Images/Skills/Server/ruby.png',
-    '../../public/Images/Skills/Server/php.png',
-    '../../public/Images/Skills/Server/scala.png'
-  ],
-  database: [
-    '../../public/Images/Skills/Database/oracle.png',
-    '../../public/Images/Skills/Database/mysql.png',
-    '../../public/Images/Skills/Database/mongo.png',
-    '../../public/Images/Skills/Database/redis.png',
-    '../../public/Images/Skills/Database/elastic.png',
-    '../../public/Images/Skills/Database/accumulo.png',
-    '../../public/Images/Skills/Database/access.png'
-  ],
-  devops: [
-    '../../public/Images/Skills/Devops/ansible.png',
-    '../../public/Images/Skills/Devops/docker.png',
-    '../../public/Images/Skills/Devops/jenkins.png',
-    '../../public/Images/Skills/Devops/vault.png',
-    '../../public/Images/Skills/Devops/terraform.png'
-  ],
-  aws: [
-    '../../public/Images/Skills/Aws/developer.png',
-    '../../public/Images/Skills/Aws/sysopsadministrator.png',
-    '../../public/Images/Skills/Aws/devops.png',
-    '../../public/Images/Skills/Aws/solutionsarchitect-a.png',
-    '../../public/Images/Skills/Aws/solutionsarchitect-p.png'
-  ]
+  // server: [
+  //   '../../public/Images/Skills/Server/java.png',
+  //   '../../public/Images/Skills/Server/python.png',
+  //   '../../public/Images/Skills/Server/node.png',
+  //   '../../public/Images/Skills/Server/meteor.png',
+  //   '../../public/Images/Skills/Server/ruby.png',
+  //   '../../public/Images/Skills/Server/php.png',
+  //   '../../public/Images/Skills/Server/scala.png'
+  // ],
+  // database: [
+  //   '../../public/Images/Skills/Database/oracle.png',
+  //   '../../public/Images/Skills/Database/mysql.png',
+  //   '../../public/Images/Skills/Database/mongo.png',
+  //   '../../public/Images/Skills/Database/redis.png',
+  //   '../../public/Images/Skills/Database/elastic.png',
+  //   '../../public/Images/Skills/Database/accumulo.png',
+  //   '../../public/Images/Skills/Database/access.png'
+  // ],
+  // devops: [
+  //   '../../public/Images/Skills/Devops/ansible.png',
+  //   '../../public/Images/Skills/Devops/docker.png',
+  //   '../../public/Images/Skills/Devops/jenkins.png',
+  //   '../../public/Images/Skills/Devops/vault.png',
+  //   '../../public/Images/Skills/Devops/terraform.png'
+  // ],
+  // aws: [
+  //   '../../public/Images/Skills/Aws/developer.png',
+  //   '../../public/Images/Skills/Aws/sysopsadministrator.png',
+  //   '../../public/Images/Skills/Aws/devops.png',
+  //   '../../public/Images/Skills/Aws/solutionsarchitect-a.png',
+  //   '../../public/Images/Skills/Aws/solutionsarchitect-p.png'
+  // ]
 }
 
 const PHOTOGRAPHY = [{
