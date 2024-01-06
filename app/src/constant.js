3a4881114f611d6ff77ef6e64c3ad5220225eb44
aadil42 <adilkelawala@gmail.com>
2024-01-06 16:57:27 +0530

    add documents and images

--- app/src/constant.js
+++ app/src/constant.js
@@ -3,50 +3,15 @@ const INFO = {
   email: 'adilkelawala@gmail.com',
   summary: 'Software Engineer',
   profession: 'Software Engineer',
-  resume: '../../public/Documents/Resume.pdf'
+  resume: '/Documents/Resume.pdf'
 }
 
 const SKILLS = {
   client: [
-    '../../public/Images/Skills/Client/html.png',
-    '../../public/Images/Skills/Client/css.png',
-    '../../public/Images/Skills/Client/react.png',
-    // '../../public/Images/Skills/Client/angular.png',
-    // '../../public/Images/Skills/Client/vue.png',
-    // '../../public/Images/Skills/Client/blaze.png'
-  ],
-  // server: [
-  //   '../../public/Images/Skills/Server/java.png',
-  //   '../../public/Images/Skills/Server/python.png',
-  //   '../../public/Images/Skills/Server/node.png',
-  //   '../../public/Images/Skills/Server/meteor.png',
-  //   '../../public/Images/Skills/Server/ruby.png',
-  //   '../../public/Images/Skills/Server/php.png',
-  //   '../../public/Images/Skills/Server/scala.png'
-  // ],
-  // database: [
-  //   '../../public/Images/Skills/Database/oracle.png',
-  //   '../../public/Images/Skills/Database/mysql.png',
-  //   '../../public/Images/Skills/Database/mongo.png',
-  //   '../../public/Images/Skills/Database/redis.png',
-  //   '../../public/Images/Skills/Database/elastic.png',
-  //   '../../public/Images/Skills/Database/accumulo.png',
-  //   '../../public/Images/Skills/Database/access.png'
-  // ],
-  // devops: [
-  //   '../../public/Images/Skills/Devops/ansible.png',
-  //   '../../public/Images/Skills/Devops/docker.png',
-  //   '../../public/Images/Skills/Devops/jenkins.png',
-  //   '../../public/Images/Skills/Devops/vault.png',
-  //   '../../public/Images/Skills/Devops/terraform.png'
-  // ],
-  // aws: [
-  //   '../../public/Images/Skills/Aws/developer.png',
-  //   '../../public/Images/Skills/Aws/sysopsadministrator.png',
-  //   '../../public/Images/Skills/Aws/devops.png',
-  //   '../../public/Images/Skills/Aws/solutionsarchitect-a.png',
-  //   '../../public/Images/Skills/Aws/solutionsarchitect-p.png'
-  // ]
+    '/Images/Skills/Client/html.png',
+    '/Images/Skills/Client/css.png',
+    '/Images/Skills/Client/react.png',
+  ]
 }
 
 const PHOTOGRAPHY = [{
@@ -73,11 +38,11 @@ const PHOTOGRAPHY = [{
 
 const CONTACT = [{
   media: 'Linkedin',
-  path: 'https://linkedin.com/in/ayyaz-a-88844749',
+  path: 'https://www.linkedin.com/in/aadil-k-525717199/',
   icon: 'fab fa-linkedin-in'
 }, {
   media: 'Github',
-  path: 'https://github.com/aakhtar3',
+  path: 'https://github.com/aadil42',
   icon: 'fab fa-github'
 }, {
   media: 'Dev.tO',
