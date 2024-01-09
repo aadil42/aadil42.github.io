c4b9eac6a44fe590ee99b093973c3bb090fb4209
aadil42 <adilkelawala@gmail.com>
2024-01-09 15:31:50 +0530

    fix path to resume

--- app/src/constant.js
+++ app/src/constant.js
@@ -3,7 +3,7 @@ const INFO = {
   email: 'adilkelawala@gmail.com',
   summary: 'Software Engineer',
   profession: 'Software Engineer',
-  resume: '../../Documents/Resume.pdf'
+  resume: '../../public/Documents/Resume.pdf'
 }
 
 const SKILLS = {
